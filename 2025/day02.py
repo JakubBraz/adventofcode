5de1bcc577118f6bfd74a67baa99cf6f11b9d918
Jakub Braz <jakubbr@gmail.com>
2025-12-02 06:42:37 +0100

    day2

---
+++ 2025/day02.py
@@ -0,0 +1,32 @@
+"""
+My result:
+ 2    852   1575  **
+"""
+
+from inputs import inp02
+import math
+
+def parse_input(inp):
+    inp = inp.split(",")
+    inp = [(int(x[0]), int(x[1])) for x in [z.split("-") for z in inp]]
+    return inp
+
+def is_valid(x, all_values):
+    x = str(x)
+    l = len(x) // 2
+    r = range(1, l + 1) if all_values else range(math.ceil(len(x) / 2.0), l + 1)
+    for i in r:
+        if len(x) % i == 0 and x.count(x[:i]) * i == len(x) == len(x):
+            return False
+    return True
+
+def solve(inp, all_values):
+    result = 0
+    for a, b in inp:
+        for x in range(a, b + 1):
+            if not is_valid(x, all_values):
+                result += x
+    return result
+
+print(solve(parse_input(inp02.raw_input), False))
+print(solve(parse_input(inp02.raw_input), True))
